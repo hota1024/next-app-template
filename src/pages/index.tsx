0bc0c489f327a7fcf79ebfab4b4987e26bbf5314
hota1024 <hotalog@hota1024.com>
2021-02-02 09:37:33 +0900

    🏷️ setup typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - __ctp__name</title>
+      </Head>
+
+      <div>__ctp__name</div>
+    </>
+  )
+}
+
+export default HomePage
